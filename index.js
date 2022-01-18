119ef1438fb6d5673c307a9895daf0afd61e4ca0
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-01-18 16:55:32 +0100

    exported the Js files and used it in a different module

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+//importing the files
+const firstFile = require('./utilities/file-one');
+const secondFile = require('./utilities/file-two');
+
+// Destructuring the files
+const {largeNumber, cut3} = firstFile;
+const {sum, concat} = secondFile;
+
+
+const arr = [1,2,3,4,5,6,7,8,9];
+const arr2 = [10,11,12,14,15]
+
+
+console.log(largeNumber(arr));
+console.log(cut3(arr));
+console.log(sum(arr));
+console.log(concat(arr, arr2));
 
